da8d0f7261026d87ce96c986c37b1c7521086405
shulandmimi <sshuang141@163.com>
2022-09-04 00:53:06 +0800

    feat: 补充棋格的枚举值

--- src/chess/chess.ts
+++ src/chess/chess.ts
@@ -4,14 +4,20 @@ import { Pieces, PIECE_ENUM_MAP_STR, PIECE_STR_MAP_ENUM } from './piece';
 
 const attack_map_static = genernal_attack_map_static();
 
-export type Square = number;
+export type Square = keyof typeof SQUARE_MAP;
 
 export const FEN_START = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR';
 
+// prettier-ignore
 export const SQUARE_MAP = {
-    a8: 0,
-    a7: 8,
-    a1: 56,
+    a8: 0,  b8: 1,  c8: 2,  d8: 3,  e8: 4,  f8: 5,  g8: 6,  h8: 7,
+    a7: 8,  b7: 9,  c7: 10, d7: 11, e7: 12, f7: 13, g7: 14, h7: 15,
+    a6: 16, b6: 17, c6: 18, d6: 19, e6: 20, f6: 21, g6: 22, h6: 23,
+    a5: 24, b5: 25, c5: 26, d5: 27, e5: 28, f5: 29, g5: 30, h5: 31,
+    a4: 32, b4: 33, c4: 34, d4: 35, e4: 36, f4: 37, g4: 38, h4: 39,
+    a3: 40, b3: 41, c3: 42, d3: 43, e3: 44, f3: 45, g3: 46, h3: 47,
+    a2: 48, b2: 49, c2: 50, d2: 51, e2: 52, f2: 53, g2: 54, h2: 55,
+    a1: 56, b1: 57, c1: 58, d1: 59, e1: 60, f1: 61, g1: 62, h1: 63,
 };
 
 export class Chess {
@@ -39,6 +45,8 @@ export class Chess {
                 } else if (PIECE_STR_MAP_ENUM[row[j]]) {
                     const piece = row[j];
                     pieces[i * 8 + j] = PIECE_STR_MAP_ENUM[piece];
+                } else {
+                    throw new SyntaxError('FEN char syntax error');
                 }
             }
         }
@@ -49,8 +57,9 @@ export class Chess {
     move({ from, to }: { from: Square; to: Square }) {
         const white = !(this.position.player & Color.black);
 
+        const [from_square, to_square] = [SQUARE_MAP[from], SQUARE_MAP[to]];
         // 获取需要移动的棋子
-        const piece = this.position.piece_at(from);
+        const piece = this.position.piece_at(from_square);
 
         if (!piece) return null;
 
@@ -61,24 +70,24 @@ export class Chess {
         ];
 
         // 获取到当前棋子的移动路径
-        const attack_map = attack_map_static[Math.abs(piece) as Pieces][from];
+        const attack_map = attack_map_static[Math.abs(piece) as Pieces][from_square];
 
         // 是否可以捕获到棋子
-        const can_capture = attack_map & (1n << BigInt(to)) & to_board.value;
+        const can_capture = attack_map & (1n << BigInt(to_square)) & to_board.value;
 
         if (can_capture) console.log('can capture');
         else console.log('move');
 
         // 可捕获状态下，消除对方棋子
         if (can_capture) {
-            const to_piece = this.position.piece_at(to)!;
+            const to_piece = this.position.piece_at(to_square)!;
             // 在可吃子的情况下，删除敌方棋子
-            this.position.remove_piece(to_piece, to);
+            this.position.remove_piece(to_piece, to_square);
         }
 
         // 添加我方棋子到目的地
-        this.position.remove_piece(piece, from);
-        this.position.add_piece(piece, to);
+        this.position.remove_piece(piece, from_square);
+        this.position.add_piece(piece, to_square);
     }
 
     ascii() {
